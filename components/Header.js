5268bc92c845eba09b0671e3517ffb1fd08b168c
SufiyanRauf <sufiyan.s.rauf@gmail.com>
2025-08-01 16:39:38 -0700

    Move navbar into Header component

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,32 @@
 "use client";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography, Container } from "@mui/material";
 
 export default function Header() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, color: "black" }}
-        >
-          Flashcard AI
-        </Typography>
-        <SignedOut>
-          <Button color="inherit" href="/sign-in">
-            Login
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Toolbar>
+    <AppBar position="sticky" color="transparent" elevation={0}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontWeight: 700, color: 'text.primary' }}
+          >
+            SparkCards
+          </Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">
+              Login
+            </Button>
+            <Button variant="contained" href="/sign-up" sx={{ ml: 2 }}>
+              Sign Up
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
